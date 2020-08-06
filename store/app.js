f363696e4ab06bc7e36cb8deb41beeff682fd534
giorgio <assistenza@ict-progetti.it>
2020-08-06 10:42:14 +0200

    ...

--- store/app.js
+++ store/app.js
@@ -9,7 +9,7 @@ export const state = () => ({
     { icon: 'mdi-home', title: 'Home', to: '/', drawer: 'true', tabmenu: 'true'  },
     { icon: 'mdi-folder', title: 'Lavori', to: '/gestione_lavori', drawer: 'true', tabmenu: 'true'  }, // , { icon: 'mdi-monitor-eye', title: 'monitor', to: '/monitor'  },
     { icon: 'mdi-contacts', title: 'Contatti', to: '/gestione_contatti', drawer: 'true', tabmenu: 'false'  },
-    { icon: 'import_contacts', title: 'Cataloghi', to: '/cataloghi', drawer: 'true', tabmenu: 'true'  },
+    { icon: 'mdi-book-open-blank-variant', title: 'Cataloghi', to: '/cataloghi', drawer: 'true', tabmenu: 'true'  },
     { icon: 'mdi-account-group', title: 'Utenti', to: '/gestione_utenti', drawer: 'true', tabmenu: 'false'  },
     { icon: 'mdi-database-sync', title: 'Sincronizzazione', to: '/sincronizzazione', drawer: 'true', tabmenu: 'false'}
   ],
